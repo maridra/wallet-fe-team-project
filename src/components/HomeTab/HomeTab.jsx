33ec54352d237b47745af4e9f9a6811afecd707b
Leon7422 <y.kurduban98@gmail.com>
2023-01-25 19:07:07 +0200

    Mobile markup ready

--- src/components/HomeTab/HomeTab.jsx
+++ src/components/HomeTab/HomeTab.jsx
@@ -20,7 +20,7 @@ const backEnd = [
   },
   {
     Date: '04.05.2021',
-    Type: '-',
+    Type: '+',
     Category: 'Other',
     Comment: 'Expence',
     Sum: '5000',
@@ -28,33 +28,41 @@ const backEnd = [
   },
 ];
 
+function userColorUi(item) {
+  if (item.Type === '+') {
+    return `${s.operationInfo} ${s.operationInfoIncome}`;
+  } else {
+    return `${s.operationInfo} ${s.operationInfoExpence}`;
+  }
+}
+
 const HomeTab = () => {
   return (
     <ul className={s.operationList}>
       {backEnd.map(item => (
         <li className={s.operationItem}>
-          <ul>
-            <li className={s.operationInfo}>
+          <ul className={s.operationItemUl}>
+            <li className={userColorUi(item)}>
               <span className={s.mobileTableHeader}>Date</span>
               <span>{item.Date}</span>
             </li>
-            <li className={s.operationInfo}>
+            <li className={userColorUi(item)}>
               <span className={s.mobileTableHeader}>Type</span>
               <span>{item.Type}</span>
             </li>
-            <li className={s.operationInfo}>
+            <li className={userColorUi(item)}>
               <span className={s.mobileTableHeader}>Category</span>
               <span>{item.Category}</span>
             </li>
-            <li className={s.operationInfo}>
+            <li className={userColorUi(item)}>
               <span className={s.mobileTableHeader}>Comment</span>
               <span>{item.Comment}</span>
             </li>
-            <li className={s.operationInfo}>
+            <li className={userColorUi(item)}>
               <span className={s.mobileTableHeader}>Sum</span>
               <span>{item.Sum}</span>
             </li>
-            <li className={s.operationInfo}>
+            <li className={userColorUi(item)}>
               <span className={s.mobileTableHeader}>Balance</span>
               <span>{item.Balance}</span>
             </li>
