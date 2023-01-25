70f15c5ebd700a418d48b0fe9b7e918fe5e90ed0
Leon7422 <y.kurduban98@gmail.com>
2023-01-25 16:45:11 +0200

    In progress

--- src/components/HomeTab/HomeTab.jsx
+++ src/components/HomeTab/HomeTab.jsx
@@ -1,19 +1,67 @@
 import { Outlet } from 'react-router-dom';
 import s from './HomeTab.module.scss';
 
+const backEnd = [
+  {
+    Date: '04.05.2021',
+    Type: '-',
+    Category: 'Other',
+    Comment: 'Expence',
+    Sum: '5000',
+    Balance: '10000',
+  },
+  {
+    Date: '04.05.2021',
+    Type: '-',
+    Category: 'Other',
+    Comment: 'Expence',
+    Sum: '5000',
+    Balance: '10000',
+  },
+  {
+    Date: '04.05.2021',
+    Type: '-',
+    Category: 'Other',
+    Comment: 'Expence',
+    Sum: '5000',
+    Balance: '10000',
+  },
+];
+
 const HomeTab = () => {
   return (
-    <table>
-      <tr>
-        <th className={s.test}>Date</th>
-        <th>Type</th>
-        <th>Category</th>
-        <th>Comment</th>
-        <th>Sum</th>
-        <th>Balance</th>
-      </tr>
-      <Outlet />
-    </table>
+    <ul className={s.operationList}>
+      {backEnd.map(item => (
+        <li className={s.operationItem}>
+          <ul>
+            <li className={s.operationInfo}>
+              <span className={s.mobileTableHeader}>Date</span>
+              <span>{item.Date}</span>
+            </li>
+            <li className={s.operationInfo}>
+              <span className={s.mobileTableHeader}>Type</span>
+              <span>{item.Type}</span>
+            </li>
+            <li className={s.operationInfo}>
+              <span className={s.mobileTableHeader}>Category</span>
+              <span>{item.Category}</span>
+            </li>
+            <li className={s.operationInfo}>
+              <span className={s.mobileTableHeader}>Comment</span>
+              <span>{item.Comment}</span>
+            </li>
+            <li className={s.operationInfo}>
+              <span className={s.mobileTableHeader}>Sum</span>
+              <span>{item.Sum}</span>
+            </li>
+            <li className={s.operationInfo}>
+              <span className={s.mobileTableHeader}>Balance</span>
+              <span>{item.Balance}</span>
+            </li>
+          </ul>
+        </li>
+      ))}
+    </ul>
   );
 };
 
