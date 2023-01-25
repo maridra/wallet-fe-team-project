bc71ae03fb75872711434bfde63081e468590315
Leon7422 <y.kurduban98@gmail.com>
2023-01-25 12:50:33 +0200

    Start

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,12 @@
+import HomeTab from 'components/HomeTab/HomeTab';
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <>
+      <div>HomePage</div>
+      <HomeTab />
+    </>
+  );
 };
 
 export default HomePage;
