e58c218ac486b56252d66c2c30023e40f0087012
Vialii Hordiichuk <vgordiychuk5@gmail.com>
2023-01-25 21:46:48 +0200

    header-2

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,25 +10,21 @@ const Header = () => {
   return (
     <header>
       <div className={s.container}>
-        <section className={s.header}>
-          <Link to="" className={s.link}>
-            <Logo svg={s.link__logo} />
-            <p className={s.link__title}>Wallet</p>
-          </Link>
-          <div className={s.wrapper}>
-            <div className={s.user}>
-              <span className={s.user__name}>Name</span>
-            </div>
-            <IconContext.Provider
-              value={{ className: 'global-class-name', size: '24px' }}
-            >
-              <button className={s.logout__button} type="button" onClick={null}>
-                <IoExitOutline />
-                <span className={s.logout__text}>Exit</span>
-              </button>
-            </IconContext.Provider>
+        <Link to="/" className={s.link}>
+          <Logo svg={s.link__logo} />
+          <p className={s.link__title}>Wallet</p>
+        </Link>
+        <div className={s.wrapper}>
+          <div className={s.user}>
+            <span className={s.user__name}>Name</span>
           </div>
-        </section>
+          <IconContext.Provider value={{ size: '24px' }}>
+            <button className={s.logout__button} type="button" onClick={null}>
+              <IoExitOutline />
+              <span className={s.logout__text}>Exit</span>
+            </button>
+          </IconContext.Provider>
+        </div>
       </div>
     </header>
   );
