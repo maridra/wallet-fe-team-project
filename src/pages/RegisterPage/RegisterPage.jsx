2f0803eaa3a9d13d4e363a5cd18c96e8bd53823a
Eugenia Kutsel <jenna.kutsel@gmail.com>
2023-01-26 19:46:08 +0200

    Update styles

--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -6,42 +6,45 @@ import registerTabImg from "../../image/registerTabImg.png";
 
 const RegisterPage = () => {
   return (
-    <div className={s.container}>
-      <div className={s.wrapper}>
-        <Media queries={{
-          tab: "(min-width: 768px) and (max-width: 1279px)",
-          desk: "(min-width: 1280px)"
-        }}>
-          {matches => (
-            <>
-              {matches.tab && (
-                <div className={s.registerImgWrapper}>
-                  <img
-                    src={registerTabImg}
-                    alt="The phone with app on the screen"
-                    className={s.registerImg}
-                  />
-                  <p className={s.appTitle}>Finance App</p>
-                </div>
-              )}
-              {matches.desk && (
-                <div className={s.registerImgWrapper}>
-                  <img
-                    src={registerDeskImg}
-                    alt="The phone with app on the screen"
-                    className={s.registerImg}
-                  />
-                  <p className={s.appTitle}>Finance App</p>
-                </div>
-              )}
-            </>
-          )}
-        </Media>
-      </div>
-      <div className={s.form}>
-        <RegisterForm />
+    <div className={s.back}>
+      <div className={s.container}>
+        <div className={s.registerContainer}>
+          <Media queries={{
+            tab: "(min-width: 768px) and (max-width: 1279px)",
+            desk: "(min-width: 1280px)"
+          }}>
+            {matches => (
+              <>
+                {matches.tab && (
+                  <div className={s.registerImgWrapper}>
+                    <img
+                      src={registerTabImg}
+                      alt="The phone with app on the screen"
+                      className={s.registerImg}
+                    />
+                    <p className={s.appTitle}>Finance App</p>
+                  </div>
+                )}
+                {matches.desk && (
+                  <div className={s.registerImgWrapper}>
+                    <img
+                      src={registerDeskImg}
+                      alt="The phone with app on the screen"
+                      className={s.registerImg}
+                    />
+                    <p className={s.appTitle}>Finance App</p>
+                  </div>
+                )}
+              </>
+            )}
+          </Media>
+          <div className={s.form}>
+            <RegisterForm />
+          </div>
+        </div>
       </div>
     </div>
+    
   )
 }
 
