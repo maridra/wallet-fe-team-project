503d42bb26bbf7d026c248a3a0b18c0b4814e0f2
Mykola Berezhniuk <bermukola@gmail.com>
2023-01-26 17:22:42 +0200

    base styles in notFound page

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,3 +1,23 @@
+import s from '../NotFound/NotFound.module.scss';
+
+import sadLeopardIcon from '../../assets/Images/NotFoundPage/sad_leopard.png';
+
+console.log(s);
+
 export default function NotFound() {
-  return <h2 style={{ color: 'red' }}>NotFound</h2>;
+  return (
+    <>
+      <div className={s.bg_page}>
+        <div className={s.bg_blur}>
+          <div className={s.container}>
+            <div className={s.wrapper}>
+              <img className={s.img} src={sadLeopardIcon} alt="sad leopard" />
+              <h2 className={s.title}>NotFound</h2>
+              <p className={s.desc}>NotFound</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
