94b9f2068376faac8bf9746f17b2279fa7251a66
Buzanov Kostya <buzanov.kostya@gmail.com>
2023-01-26 16:01:48 +0200

    Update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,6 @@
 import { lazy, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Header, Loader } from '../components';
-import { useSelector } from 'react-redux';
-import { modalSelectors } from 'redux/modal/modalSelectors';
-import ModalAddTransaction from './Modal/ModalAddTransaction/ModalAddTransaction';
 
 // import PrivateRoute from './PrivateRoute/PrivateRoute';
 import PublicRoute from './PublicRoute/PublicRoute';
@@ -13,15 +10,11 @@ const NotFound = lazy(() => import('../pages/NotFound/NotFound'));
 const RegisterPage = lazy(() => import('../pages/RegisterPage/RegisterPage'));
 
 export const App = () => {
-  const showModalAddTransaction = useSelector(
-    modalSelectors.showModalAddTransaction
-  );
   return (
     <>
       <Suspense
         fallback={<Loader height={'120'} width={'120'} color={'#4A56E2'} />}
       >
-        {showModalAddTransaction && <ModalAddTransaction />}
         <Routes>
           <Route path="/" element={<Header />}>
             <Route index element={<HomePage />} />
