286c3582f0f2a84ffa1c34dc05b42af989d00023
Mykola Berezhniuk <bermukola@gmail.com>
2023-01-26 11:33:50 +0200

    fix header and add base fond

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 // import { useSelector, useDispatch } from 'react-redux';
 import { IconContext } from 'react-icons';
 import { IoExitOutline } from 'react-icons/io5';
@@ -8,25 +8,28 @@ import s from './Header.module.scss';
 
 const Header = () => {
   return (
-    <header>
-      <div className={s.container}>
-        <Link to="/" className={s.link}>
-          <Logo svg={s.link__logo} />
-          <p className={s.link__title}>Wallet</p>
-        </Link>
-        <div className={s.wrapper}>
-          <div className={s.user}>
-            <span className={s.user__name}>Name</span>
+    <>
+      <header>
+        <div className={s.container}>
+          <Link to="/" className={s.link}>
+            <Logo svg={s.link__logo} />
+            <p className={s.link__title}>Wallet</p>
+          </Link>
+          <div className={s.wrapper}>
+            <div className={s.user}>
+              <span className={s.user__name}>Name</span>
+            </div>
+            <IconContext.Provider value={{ size: '24px' }}>
+              <button className={s.logout__button} type="button" onClick={null}>
+                <IoExitOutline />
+                <span className={s.logout__text}>Exit</span>
+              </button>
+            </IconContext.Provider>
           </div>
-          <IconContext.Provider value={{ size: '24px' }}>
-            <button className={s.logout__button} type="button" onClick={null}>
-              <IoExitOutline />
-              <span className={s.logout__text}>Exit</span>
-            </button>
-          </IconContext.Provider>
         </div>
-      </div>
-    </header>
+      </header>
+      <Outlet />
+    </>
   );
 };
 export default Header;
