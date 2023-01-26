e32d822fd68dad8bf38dd9f4152763e9df22ef47
Buzanov Kostya <buzanov.kostya@gmail.com>
2023-01-26 18:39:15 +0200

    create submit and validation

--- src/components/Modal/ModalAddTransaction/ModalAddTransactionForm/ModalAddTransactionForm.jsx
+++ src/components/Modal/ModalAddTransaction/ModalAddTransactionForm/ModalAddTransactionForm.jsx
@@ -1,72 +1,44 @@
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as yup from 'yup';
 import Datetime from 'react-datetime';
-// import { useState } from 'react';
+import { useState } from 'react';
 import 'react-datetime/css/react-datetime.css';
 import { RiCalendar2Line } from 'react-icons/ri';
+import getDate from 'utils/getDate';
 import scss from './ModalAddTransactionForm.module.scss';
 
 const schema = yup.object().shape({
   sum: yup.number().min(0.01).max(2500000).required(),
-  date: yup.string().required(),
 });
 const initialValues = {
   category: '',
   sum: '',
 };
+const initialValuesTwo = {
+  sum: '',
+};
 
 const ModalAddTransactionForm = prop => {
   const { checkboxStatus } = prop;
-  // const [sum, setSum] = useState('');
+  const [date, setDate] = useState(getDate());
 
-  // const getDate = e => {
-  //   const oldDate = new Date(Date.parse(e._d));
-  //   let dd = oldDate.getDate();
-  //   if (dd < 10) {
-  //     dd = '0' + dd;
-  //   }
-  //   let mm = oldDate.getMonth() + 1;
-  //   if (mm < 10) {
-  //     mm = '0' + mm;
-  //   }
-  //   let yy = oldDate.getFullYear();
-
-  //   const newDate = dd + '.' + mm + '.' + yy;
-
-  //   return setDate(newDate);
-  // };
-
-  const handleSubmit = async e => {
-    e.preventDefault();
-
-    const form = e.currentTarget;
-
-    if (!form.elements.category) {
-      const formValues = {
-        sum: form.elements.sum.value,
-        date: form.elements.date.value,
-      };
-      const isValid = await schema.isValid({
-        sum: form.elements.sum.value,
-        date: form.elements.date.value,
-      });
-
-      console.log(isValid);
-      if (isValid) {
-        return console.log(formValues);
-      }
-      return alert('hi');
-    } else {
-      const formValues = {
-        category: form.elements.category.value,
-        sum: form.elements.sum.value,
-        date: form.elements.date.value,
-      };
-      return console.log(formValues);
-    }
+  const createDate = date => {
+    setDate(getDate(date));
   };
 
-  const renderInput = (props, openCalendar) => {
+  const handleSubmit = (values, { resetForm }) => {
+    const { category, sum } = values;
+    if (category === '') {
+      const formValues = { sum, date };
+      console.log(formValues);
+      return resetForm();
+    }
+    const formValues = { ...values, date };
+    console.log(formValues);
+    return resetForm();
+  };
+
+  const renderCalendarInput = (props, openCalendar) => {
     return (
       <div className={scss.dataBox}>
         <Field
@@ -86,8 +58,12 @@ const ModalAddTransactionForm = prop => {
   };
 
   return (
-    <Formik initialValues={initialValues} validationSchema={schema}>
-      <Form className={scss.calculatorForm} onSubmit={handleSubmit}>
+    <Formik
+      initialValues={!checkboxStatus ? initialValues : initialValuesTwo}
+      validationSchema={schema}
+      onSubmit={handleSubmit}
+    >
+      <Form className={scss.calculatorForm}>
         <div className={scss.calculatorFormInputContainer}>
           {!checkboxStatus && (
             <label>
@@ -97,7 +73,6 @@ const ModalAddTransactionForm = prop => {
                 placeholder="Select a category"
                 name="category"
               ></Field>
-
               <ErrorMessage
                 className={scss.errorMessage}
                 name="category"
@@ -122,11 +97,11 @@ const ModalAddTransactionForm = prop => {
           <label>
             <Datetime
               timeFormat={false}
-              renderInput={renderInput}
+              renderInput={renderCalendarInput}
               dateFormat="DD.MM.YYYY"
               closeOnSelect={true}
               initialValue={new Date()}
-              // onChange={getDate}
+              onChange={createDate}
             />
           </label>
           <label>
