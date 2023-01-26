48bcf1914e328b9c5845b8f203e52b632d63d8cf
Mykola Berezhniuk <bermukola@gmail.com>
2023-01-26 17:54:26 +0200

    add slyled link

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import s from '../NotFound/NotFound.module.scss';
 
 import sadLeopardIcon from '../../assets/Images/NotFoundPage/sad_leopard.png';
@@ -12,8 +13,14 @@ export default function NotFound() {
           <div className={s.container}>
             <div className={s.wrapper}>
               <img className={s.img} src={sadLeopardIcon} alt="sad leopard" />
-              <h2 className={s.title}>NotFound</h2>
-              <p className={s.desc}>NotFound</p>
+              <h2 className={s.title}>Opps... Page Not Found!</h2>
+              <p className={s.desc}>
+                But does it matter if leopards are already in Ukraine? Click{' '}
+                <Link to="/" className={s.link}>
+                  here{' '}
+                </Link>{' '}
+                to get back to home page.
+              </p>
             </div>
           </div>
         </div>
