1b24e5a5cdc2e06d55f337addb83bfaab1c152db
Leon7422 <y.kurduban98@gmail.com>
2023-01-27 13:45:35 +0200

    Update financeOperations.js

--- src/redux/finance/financeOperations.js
+++ src/redux/finance/financeOperations.js
@@ -6,7 +6,7 @@ const updateTransactions = createAsyncThunk(
   async (_, thunkAPI) => {
     try {
       token.set(
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzZDI3MTM3ODgzY2EwM2E1NDZjOThmNyIsImlhdCI6MTY3NDgxNzgxNSwiZXhwIjoxNjc0ODIxNDE1fQ.xugHS3HEPBmYgFylUI860bhogqsNd62YzIfaOQpEwgA'
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzZDI3MTM3ODgzY2EwM2E1NDZjOThmNyIsImlhdCI6MTY3NDgxOTg4MiwiZXhwIjoxNjc0ODIzNDgyfQ.l1NYjK6QINzNmeD55hNYLBgFqM7k-ltozaZFV6bxOEA'
       );
       const { data } = await axiosBaseUrl.get('transactions');
 
