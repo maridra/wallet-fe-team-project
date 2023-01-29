6a1b7bd6e0e82e0db4148219486533b841e7094c
Mykola Berezhniuk <bermukola@gmail.com>
2023-01-29 17:40:10 +0200

    add private route

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,7 @@ import { lazy, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Header, Loader } from '../components';
 
-// import PrivateRoute from './PrivateRoute/PrivateRoute';
+import PrivateRoute from './PrivateRoute/PrivateRoute';
 import PublicRoute from './PublicRoute/PublicRoute';
 
 const HomePage = lazy(() => import('../pages/HomePage/HomePage'));
@@ -28,14 +28,17 @@ export const App = () => {
         }
       >
         <Routes>
-          <Route path="/" element={<Header />}>
+          <Route
+            path="/"
+            element={<PrivateRoute redirectTo="/login" children={<Header />} />}
+          >
             <Route path="/" element={<HomePage />}>
               <Route path="/statistic" element={<StatisticPage />} />
               <Route path="/currency" element={<CurrencyPage />} />
             </Route>
             <Route path="/settings" element={<SettingsPage />} />
           </Route>
-          <Route path="*" element={<NotFound />} />
+
           <Route
             path="/signUp"
             element={<PublicRoute redirectTo="/" children={<RegisterPage />} />}
@@ -44,6 +47,7 @@ export const App = () => {
             path="/login"
             element={<PublicRoute redirectTo="/" children={<LoginPage />} />}
           />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </Suspense>
     </>
