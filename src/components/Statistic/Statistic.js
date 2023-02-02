c0a3d99a1305cfc55eb0bcb2fe121d39fdadab5c
prorochenko <33101252+prorochenko@users.noreply.github.com>
2023-02-02 20:12:42 +0200

    added show all transactions for all period

--- src/components/Statistic/Statistic.js
+++ src/components/Statistic/Statistic.js
@@ -3,20 +3,20 @@ import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 import { v4 as uuidv4 } from 'uuid';
 import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { getStatistic } from '../../redux/statistic/statisticOperation';
 import { connect } from 'react-redux';
-import { statisticSelectors } from 'redux/statistic/statisticSelectors';
-import scss from './Statistic.module.scss';
-import { SelectForStatisticMonth, SelectForStatisticYear } from './Select';
-import { FiChevronDown, FiChevronUp } from 'react-icons/fi';
 import { IconContext } from 'react-icons';
+import { IoStatsChartOutline, IoStatsChartSharp } from 'react-icons/io5';
+
+import { SelectElements } from './SelectElements';
+import scss from './Statistic.module.scss';
+import { getStatistic } from '../../redux/statistic/statisticOperation';
+import { statisticSelectors } from 'redux/statistic/statisticSelectors';
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 const options = {
   title: {
     display: true,
-    text: 'My Doughnut Chart',
     fontSize: 60,
     verticalAlign: 'middle',
     floating: true,
@@ -51,27 +51,15 @@ const StatisticForm = () => {
   const [newMonth, setNewMonth] = useState(date.getMonth());
   const [openMonth, setOpenMonth] = useState(false);
   const [openYear, setOpenYear] = useState(false);
+  const [openAllTransactions, setOpenAllTransactions] = useState(false);
 
   const handleOpenMonthSelect = () => {
     setOpenMonth(!openMonth);
     setOpenYear(false);
   };
-
-  // useEffect(() => {
-  //   document.addEventListener('click', handleClickOutside);
-  //   return () => {
-  //     document.removeEventListener('click', handleClickOutside);
-  //   };
-  // });
-
-  // const handleClickOutside = event => {
-  //   if (!event.target.closest('.select-for-statistic-month') && openMonth) {
-  //     setOpenMonth(false);
-  //   }
-  //   if (!event.target.closest('.select-for-statistic-year') && openYear) {
-  //     setOpenYear(false);
-  //   }
-  // };
+  const handleOpenAllTransactions = () => {
+    setOpenAllTransactions(!openAllTransactions);
+  };
 
   const handleOpenYearSelect = () => {
     setOpenYear(!openYear);
@@ -107,6 +95,20 @@ const StatisticForm = () => {
     ],
   };
 
+  const AllData = {
+    labels: stat.allExpensesByCategory.map(item => item.name),
+    datasets: [
+      {
+        label: 'Total',
+        data: stat.allExpensesByCategory.map(item => item.amount),
+        backgroundColor: stat.allExpensesByCategory.map(item => item.color),
+        borderColor: stat.allExpensesByCategory.map(item => item.color),
+        borderWidth: 1,
+        cutout: '70%',
+      },
+    ],
+  };
+
   useEffect(() => {
     dispatch(getStatistic({ month: newMonth + 1, year }));
   }, [dispatch, newMonth, year]);
@@ -116,108 +118,103 @@ const StatisticForm = () => {
         <h1 className={scss.title}>Statistics</h1>
         <div className={scss.mainBox}>
           <div className={scss.doughnut}>
-            <Doughnut data={data} options={options} />
-            <div className={scss.doughnutMonth}>
-              {stat.expensesPerMonth ? (
-                `₴ ${stat.expensesPerMonth
-                  .toLocaleString('uk-ua', {
-                    minimumFractionDigits: 2,
-                    maximumFractionDigits: 2,
-                  })
-                  .replace(/,/g, '.')}`
-              ) : (
-                <div className={scss.doughnutMonthNoValue}>₴ 0</div>
-              )}
-            </div>
+            {!openAllTransactions ? (
+              <>
+                <Doughnut data={data} options={options} />
+                <div className={scss.doughnutMonth}>
+                  {stat.expensesPerMonth ? (
+                    `₴ ${stat.expensesPerMonth
+                      .toLocaleString('uk-ua', {
+                        minimumFractionDigits: 2,
+                        maximumFractionDigits: 2,
+                      })
+                      .replace(/,/g, '.')}`
+                  ) : (
+                    <div className={scss.doughnutMonthNoValue}>₴ 0</div>
+                  )}
+                </div>
+              </>
+            ) : (
+              <>
+                <Doughnut data={AllData} options={options} />
+                <div className={scss.doughnutMonth}>
+                  {stat.expensesPerMonth ? (
+                    `₴ ${stat.expensesPerMonth
+                      .toLocaleString('uk-ua', {
+                        minimumFractionDigits: 2,
+                        maximumFractionDigits: 2,
+                      })
+                      .replace(/,/g, '.')}`
+                  ) : (
+                    <div className={scss.doughnutMonthNoValue}>₴ 0</div>
+                  )}
+                </div>
+              </>
+            )}
           </div>
           <div className={scss.statisticData}>
-            <div className={scss.select}>
-              <input
-                className={scss.selectItem}
-                type="text"
-                placeholder="Select a month"
-                name="month"
-                value={month}
-                onClick={handleOpenMonthSelect}
-                autoComplete="off"
-                readOnly
-              ></input>
-              <input
-                className={scss.selectItem}
-                type="text"
-                placeholder="Select a year"
-                name="year"
-                value={year}
-                onClick={handleOpenYearSelect}
-                autoComplete="off"
-                readOnly
-              ></input>
-              <button
-                className={scss.openMenuBtnMonth}
-                type="button"
-                onClick={handleOpenMonthSelect}
-              >
-                {!openMonth ? (
+            <button
+              className={scss.openAllTranscation}
+              type="button"
+              onClick={handleOpenAllTransactions}
+            >
+              {!openAllTransactions ? (
+                <>
                   <IconContext.Provider
                     value={{
                       size: '25px',
                     }}
                   >
-                    <FiChevronDown
-                      className={scss.openMenuBtnMonthIcon}
-                    ></FiChevronDown>
+                    <IoStatsChartSharp
+                      className={scss.openAllTranscationIcon}
+                    ></IoStatsChartSharp>
                   </IconContext.Provider>
-                ) : (
+                  <div className={scss.allTransactionText}>
+                    Show transactions for the entire period
+                  </div>
+                </>
+              ) : (
+                <>
                   <IconContext.Provider
                     value={{
                       size: '25px',
                     }}
                   >
-                    <FiChevronUp
-                      className={scss.openMenuBtnMonthIcon}
-                    ></FiChevronUp>
+                    <IoStatsChartOutline
+                      className={scss.openAllTranscationIcon}
+                    ></IoStatsChartOutline>
                   </IconContext.Provider>
-                )}
-              </button>
-              <button
-                className={scss.openMenuBtnYear}
-                type="button"
-                onClick={handleOpenYearSelect}
-              >
-                {!openYear ? (
-                  <IconContext.Provider
-                    value={{
-                      size: '25px',
-                    }}
-                  >
-                    <FiChevronDown
-                      className={scss.openMenuBtnYearIcon}
-                    ></FiChevronDown>
-                  </IconContext.Provider>
-                ) : (
-                  <IconContext.Provider
-                    value={{
-                      size: '25px',
-                    }}
-                  >
-                    <FiChevronUp
-                      className={scss.openMenuBtnYearIcon}
-                    ></FiChevronUp>
-                  </IconContext.Provider>
-                )}
-              </button>
-              {openMonth && (
-                <SelectForStatisticMonth
-                  handleMonth={handleMonthChange}
-                  // onClick={handleClickOutside}
-                ></SelectForStatisticMonth>
+                  <div>Show less</div>
+                </>
               )}
-              {openYear && (
-                <SelectForStatisticYear
-                  handleYear={handleYearChange}
-                ></SelectForStatisticYear>
-              )}
-            </div>
+            </button>
+            {!openAllTransactions ? (
+              <>
+                <SelectElements
+                  valueMonth={month}
+                  valueYear={year}
+                  onClickMonth={handleOpenMonthSelect}
+                  onClickYear={handleOpenYearSelect}
+                  openMonth={openMonth}
+                  openYear={openYear}
+                  handleMonthChange={handleMonthChange}
+                  handleYearChange={handleYearChange}
+                />
+              </>
+            ) : (
+              <div className={scss.selectClosed}>
+                <SelectElements
+                  valueMonth={month}
+                  valueYear={year}
+                  onClickMonth={handleOpenMonthSelect}
+                  onClickYear={handleOpenYearSelect}
+                  openMonth={openMonth}
+                  openYear={openYear}
+                  handleMonthChange={handleMonthChange}
+                  handleYearChange={handleYearChange}
+                />
+              </div>
+            )}
             {/* table  */}
             <table className={scss.table}>
               <thead className={scss.thead}>
@@ -227,43 +224,79 @@ const StatisticForm = () => {
                 </tr>
               </thead>
               <tbody>
-                {stat.expensesByPeriod.map(item => (
-                  <tr className={scss.tableRows} key={uuidv4()}>
-                    <td id={item.name}>
-                      <div
-                        className={scss.squareBefore}
-                        style={{ backgroundColor: `${item.color}` }}
-                      ></div>
-                      {item.name}
-                    </td>
-                    <td className={scss.tableRows__rightText}>
-                      {item.amount
-                        .toLocaleString('uk-ua', {
-                          minimumFractionDigits: 2,
-                          maximumFractionDigits: 2,
-                        })
-                        .replace(/,/g, '.')}
-                    </td>
-                  </tr>
-                ))}
+                {!openAllTransactions
+                  ? stat.expensesByPeriod.map(item => (
+                      <tr className={scss.tableRows} key={uuidv4()}>
+                        <td id={item.name}>
+                          <div
+                            className={scss.squareBefore}
+                            style={{ backgroundColor: `${item.color}` }}
+                          ></div>
+                          {item.name}
+                        </td>
+                        <td className={scss.tableRows__rightText}>
+                          {item.amount
+                            .toLocaleString('uk-ua', {
+                              minimumFractionDigits: 2,
+                              maximumFractionDigits: 2,
+                            })
+                            .replace(/,/g, '.')}
+                        </td>
+                      </tr>
+                    ))
+                  : stat.allExpensesByCategory.map(item => (
+                      <tr className={scss.tableRows} key={uuidv4()}>
+                        <td id={item.name}>
+                          <div
+                            className={scss.squareBefore}
+                            style={{ backgroundColor: `${item.color}` }}
+                          ></div>
+                          {item.name}
+                        </td>
+                        <td className={scss.tableRows__rightText}>
+                          {item.amount
+                            .toLocaleString('uk-ua', {
+                              minimumFractionDigits: 2,
+                              maximumFractionDigits: 2,
+                            })
+                            .replace(/,/g, '.')}
+                        </td>
+                      </tr>
+                    ))}
               </tbody>
               <tfoot>
                 <tr className={scss.footer}>
                   <td className={scss.tableFooter}>Expenses:</td>
                   <td className={scss.tableFooter__expenses}>
-                    {stat.expensesPerMonth
-                      ? stat.expensesPerMonth.toLocaleString('uk-ua', {
-                          minimumFractionDigits: 2,
-                          maximumFractionDigits: 2,
-                        })
+                    {!openAllTransactions && stat.expensesPerMonth
+                      ? stat.expensesPerMonth
+                          .toLocaleString('uk-ua', {
+                            minimumFractionDigits: 2,
+                            maximumFractionDigits: 2,
+                          })
+                          .replace(/,/g, '.')
+                      : openAllTransactions
+                      ? stat.totalExpenses
+                          .toLocaleString('uk-ua', {
+                            minimumFractionDigits: 2,
+                            maximumFractionDigits: 2,
+                          })
+                          .replace(/,/g, '.')
                       : 0}
                   </td>
                 </tr>
                 <tr>
                   <td className={scss.tableFooter}>Income: </td>
                   <td className={scss.tableFooter__income}>
-                    {stat.incomePerMonth
+                    {!openAllTransactions && stat.incomePerMonth
                       ? stat.incomePerMonth
+                          .toLocaleString('uk-ua', {
+                            minimumFractionDigits: 2,
+                            maximumFractionDigits: 2,
+                          })
+                          .replace(/,/g, '.')
+                      : openAllTransactions
+                      ? stat.totalIncome
                           .toLocaleString('uk-ua', {
                             minimumFractionDigits: 2,
                             maximumFractionDigits: 2,
