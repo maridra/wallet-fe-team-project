58779f2db8280041c57bb493ed85e2e36d05dd24
Maryna-Dr <drapata.mb@gmail.com>
2023-04-24 17:42:17 +0300

    Update index.js and package.json

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-  <BrowserRouter basename="/wallet-leopards-team-FRONTEND">
+  <BrowserRouter basename="/wallet-fe-team-project">
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <App />
